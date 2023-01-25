1f9735ab938df1043aba240e39517814320d3c3b
Julian Lasso <julian.lasso@sofka.com.co>
2023-01-25 11:29:56 -0500

    Código formateado

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import { ValidationPipe } from '@nestjs/common';
+// Libraries
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+// Main Module
 import { AppModule } from './app.module';
+
+// Configs
 import { PipeValidator } from './infrastructure/configs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.useGlobalPipes(new ValidationPipe(PipeValidator));
-
   await app.listen(3000);
 }
+
 bootstrap();
