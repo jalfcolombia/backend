05a214adeaf62bb4afdf6e09809dfa8e25809a14
Julian Lasso <julian.lasso@sofka.com.co>
2023-01-25 11:31:16 -0500

    Método no implementado

--- src/infrastructure/common/exception-filters/example/example.filter.ts
+++ src/infrastructure/common/exception-filters/example/example.filter.ts
@@ -2,5 +2,7 @@ import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 
 @Catch()
 export class ExampleFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+  catch(exception: T, host: ArgumentsHost) {
+    throw new Error('This method is not implemented');
+  }
 }
